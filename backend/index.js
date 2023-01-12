f3c5b201fbe94a1bb5f7ed25a44746c4f37050d0
Hamza Qureshi <m.hamzaqureshi15@gmail.com>
2023-01-12 22:21:33 +0500

    added backend directory and packages

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import  express  from "express";
+
+const app = express();
+const url = "mongodb+srv://hamza:IY8SZWJgmPHpi2Py@cluster0.96cge86.mongodb.net/?retryWrites=true&w=majority";
+
